a83996d8cdccf93cd9540f78f3283c6648935940
Abdulaziz Musaev <amusaev7225@gmail.com>
2026-01-08 15:08:59 +0100

    Solve 'Binary Tree Right Side View' problem

---
+++ 03-common-coding-patterns/12-tree-breadth-first-search/09.binary_tree_right_side_view.py
@@ -0,0 +1,37 @@
+from typing import Optional, List
+from collections import deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        queue = deque([root])
+        result = []
+
+        while queue:
+            level_size = len(queue)
+
+            for _ in range(level_size):
+                node = queue.popleft()
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(node.val)
+
+        return result
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
